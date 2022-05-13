fb3f56bb3c8cd3e9fe6666d6923158781f25aaaf
Piotr Ankus <101824477+Kojot999@users.noreply.github.com>
2022-05-13 15:37:31 +0200

    feat(wip): initial setup (#1)
    * feat(wip): initial setup
    
    * fix: some file issues

---
+++ src/app/Root/Root.js
@@ -0,0 +1,7 @@
+import "./Root.css";
+
+function Root() {
+  return <div>Hello World</div>;
+}
+
+export default Root;
---
+++ src/app/api/source.js
@@ -0,0 +1,2 @@
+export const RAWG_API =
+  "https://api.rawg.io/api/key=f9d18241ce6e4e9d9d76f31d28edf0a7";
