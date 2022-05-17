878e104e83fd23a65038dd4ed24f2bd38b7ac322
Kojot999 <piotr-ankus@wp.pl>
2022-05-17 21:59:48 +0200

    hotfix: build

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -3,7 +3,6 @@
 //   { key: "genres", value: "RPG" },
 // ];
 
-<<<<<<< HEAD
 // const API_BASE = "http://rawg.io/api";
 // const API_KEY = "DBUJIHASGVDYUHUCZX";
 
@@ -17,21 +16,6 @@
 //   const result = API_BASE + path + query + "&key=" + API_KEY;
 //   return result;
 // };
-=======
-const API_BASE = "http://rawg.io/api";
-const API_KEY = "DBUJIHASGVDYUHUCZX";
-
-const queryBuilder = (path, params) => {
-  const query = params
-    .map((param, index) => {
-      return (index > 0 ? "&" : "?") + param.key + "=" + param.value;
-    })
-    .join("");
-
-  const result = API_BASE + path + query + "&key=" + API_KEY;
-  return result;
-};
->>>>>>> main
 
 // const result = queryBuilder("/games", exampleParams);
 // console.log(result);
--- src/app/api/source.js
+++
@@ -1,2 +0,0 @@
-export const RAWG_API =
-  "https://api.rawg.io/api/key=f9d18241ce6e4e9d9d76f31d28edf0a7";
