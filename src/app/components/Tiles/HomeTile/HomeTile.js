debb4ba5d2985c6d7f26e59e4b6cfc43a2b0844a
Kojot999 <piotr-ankus@wp.pl>
2022-05-19 21:55:14 +0200

    feat: new components

--- src/app/components/Tiles/HomeTile/HomeTile.js
+++ src/app/components/Tiles/HomeTile/HomeTile.js
@@ -1,6 +1,7 @@
 import styles from "./Tile.module.scss";
 import star from "../../../../Img/star.svg";
 import metacriticIcon from "../../../../Img/metacritic.svg";
+import { PLATFORM_ICON_CONFIG } from "../../../constants/platformIcon.config";
 
 export const HomeTile = ({ data }) => {
   const {
@@ -8,7 +9,7 @@ export const HomeTile = ({ data }) => {
     name,
     metacritic,
     background_image: backgroundImage,
-    platforms,
+    parent_platforms: parentPlatforms,
     genres,
   } = data;
 
@@ -16,32 +17,43 @@ export const HomeTile = ({ data }) => {
     <div className={styles.wrapper}>
       <div key={id} className={styles.tile}>
         <div className={styles.wrapperImg}>
-          <h3 className={styles.title}>{name}</h3>
-          <img alt="" src={backgroundImage} className={styles.gameImg} />
+          <div className={styles.overlay}>
+            <img
+              alt="gameIMG"
+              src={backgroundImage}
+              className={styles.gameImg}
+            />
+          </div>
         </div>
         <img alt="star" src={star} className={styles.starIcon} />
-
+        <div className={styles.rating}>
+          <p>{metacritic}</p>
+          <img alt="metacritic" src={metacriticIcon} />
+        </div>
         <div className={styles.description}>
+          <h3 className={styles.title}>{name}</h3>
           <div className={styles.genres}>
             <ul>
-              {genres.map((genres) => (
+              {genres.slice(0, 3).map((genres) => (
                 <li key={genres.id}>
                   <a href="/">#{genres.name}</a>
                 </li>
               ))}
             </ul>
           </div>
-          <div className={styles.rating}>
-            <img alt="metacritic" src={metacriticIcon} />
-            <p>{metacritic}</p>
+          <div className={styles.platform}>
+            <ul>
+              {parentPlatforms.slice(0, 5).map(({ platform }) => (
+                <li key={platform.id}>
+                  <img
+                    alt={platform.name}
+                    src={PLATFORM_ICON_CONFIG[platform.slug]}
+                    className={styles.platformIcon}
+                  />
+                </li>
+              ))}
+            </ul>
           </div>
-        </div>
-        <div className={styles.platform}>
-          <ul>
-            {platforms.map(({ platform }) => (
-              <li>{platform.name}</li>
-            ))}
-          </ul>
         </div>
       </div>
     </div>
